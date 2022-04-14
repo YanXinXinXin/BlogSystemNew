c1f94205be4abca7a90ea8d7ece6c9971f72fc90
杨鑫鑫 <320437031@qq.com>
2022-04-14 12:35:14 +0800

    1

---
+++ JWT/Program.cs
@@ -0,0 +1,61 @@
+﻿using JWT.Algorithms;
+using JWT.Exceptions;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region 加密
+            //var payload = new Dictionary<string, object>
+            //{
+            //    {"UserId",123 },
+            //     {"UserName","admin" }
+            //};
+            //var secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk"; //秘钥
+
+            //IJwtAlgorithm algorithm = new Algorithms.HMACSHA256Algorithm();//hash256 加密算法
+            //IJsonSerializer serializer = new JsonNetSerializer();//序列化 加密器
+            //IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();//转译
+            //IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            //var token = encoder.Encode(payload, secret);//开始加密
+            //Console.WriteLine(token);
+            //#endregion
+
+
+
+            #region 解密
+            var tokenStr = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVc2VySWQiOjEyMywiVXNlck5hbWUiOiJhZG1pbjIifQ.NJBW05GZ5B0DCwL1sVkaPX4VxfxuLo4GFP8YtuErpJA";
+            var secret2 = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+            try
+            {
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer,validator,urlEncoder, algorithm);
+
+                var json = decoder.Decode(tokenStr, secret2, verify: true);
+                Console.WriteLine(json);
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+            }
+            #endregion
+        }
+    }
+}
---
+++ WebAPI/Tools/JwtTools.cs
@@ -0,0 +1,73 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Exceptions;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Tools
+{
+    public class JwtTools
+    {
+
+       readonly static  string key = System.Web.Configuration.WebConfigurationManager.AppSettings["name"];
+       /// <summary>
+       /// Jwt加密
+       /// </summary>
+       /// <param name="payload">明文部分</param>
+       /// <param name="key">秘钥</param>
+       /// <returns>返回Token</returns>
+        public static string Encode(Dictionary<string,object>payload,string key)
+        {
+
+            IJwtAlgorithm algorithm = new JWT.Algorithms.HMACSHA256Algorithm();//hash256 加密算法
+            IJsonSerializer serializer = new JsonNetSerializer();//序列化 加密器
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();//转译
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            return encoder.Encode(payload, key);//开始加密
+        }
+        /// <summary>
+        /// Jwt解密
+        /// </summary>
+        /// <param name="token">Token</param>
+        /// <param name="key"秘钥</param>
+        /// <returns>返回明文部分</returns>
+        public static string Decode(string token, string key)
+        {
+            try
+            {
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+
+                var json = decoder.Decode(token, key, verify: true);
+                return json;
+            }
+            catch (TokenExpiredException ex) 
+            {
+                Console.WriteLine("Token has expired");
+                throw;
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+                throw;
+            }
+        }
+
+
+        public static string ValideLogined(HttpRequest request) {
+            if (string.IsNullOrWhiteSpace(request.Headers["token"]))
+                throw new Exception("请重新登录");
+            return Decode(request.Headers["token"], key);
+            
+
+        }
+    }
+}
