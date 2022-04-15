6cdd63b2529c0c6db6dbb88943aa366978096d24
杨鑫鑫 <320437031@qq.com>
2022-04-16 00:37:08 +0800

    JWT

---
+++ WebAPI/Filter/Author.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using WebAPI.Tools;
+
+namespace WebAPI.Filter
+{
+    public class Author : Attribute, IAuthorizationFilter
+    {
+        public bool AllowMultiple => throw new NotImplementedException();
+
+        public async Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            if (actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Count>0)
+            {
+                return await continuation();
+            }
+            IEnumerable<string> headers;
+            if (actionContext.Request.Headers.TryGetValues("token", out headers))
+            {
+                var LoginName = JwtTools.Decode(headers.First())["LoginName"].ToString();
+                return await continuation();
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+        }
+    }
+}
--- WebAPI/Tools/JwtTools.cs
+++ WebAPI/Tools/JwtTools.cs
@@ -2,6 +2,7 @@
 using JWT.Algorithms;
 using JWT.Exceptions;
 using JWT.Serializers;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,14 @@ namespace WebAPI.Tools
     public class JwtTools
     {
 
-       public static  string key = System.Web.Configuration.WebConfigurationManager.AppSettings["name"];
+       private static  readonly string KEY = System.Web.Configuration.WebConfigurationManager.AppSettings["name"];
        /// <summary>
        /// Jwt加密
        /// </summary>
        /// <param name="payload">明文部分</param>
        /// <param name="key">秘钥</param>
        /// <returns>返回Token</returns>
-        public static string Encode(Dictionary<string,object>payload,string key)
+        public static string Encode(Dictionary<string,object>payload)
         {
 
             IJwtAlgorithm algorithm = new JWT.Algorithms.HMACSHA256Algorithm();//hash256 加密算法
@@ -28,13 +29,10 @@ namespace WebAPI.Tools
             IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();//转译
             IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
 
-            return encoder.Encode(payload, key);//开始加密
+            return encoder.Encode(payload, KEY);//开始加密
         }
-
-        internal static object ValideLogined(HttpRequestHeaders headers)
-        {
-            throw new NotImplementedException();
-        }
+       
+        
 
         /// <summary>
         /// Jwt解密
@@ -42,10 +40,11 @@ namespace WebAPI.Tools
         /// <param name="token">Token</param>
         /// <param name="key"秘钥</param>
         /// <returns>返回明文部分</returns>
-        public static string Decode(string token, string key)
+        public static Dictionary<string, object> Decode(string token)
         {
             try
             {
+
                 IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
                 IJsonSerializer serializer = new JsonNetSerializer();
                 IDateTimeProvider provider = new UtcDateTimeProvider();
@@ -53,8 +52,12 @@ namespace WebAPI.Tools
                 IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
                 IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
 
-                var json = decoder.Decode(token, key, verify: true);
-                return json;
+                var jsonStr = decoder.Decode(token, KEY, verify: true);
+                var result = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonStr);
+                if ((DateTime)result["exp"] < DateTime.Now)
+                    throw new Exception("JWT:Token已过期,请重新登录");
+                result.Remove("exp");
+                return result;
             }
             catch (TokenExpiredException ex) 
             {
@@ -68,13 +71,18 @@ namespace WebAPI.Tools
             }
         }
 
-
-        public static string ValideLogined(HttpRequest request) {
-            if (string.IsNullOrWhiteSpace(request.Headers["token"]))
-                throw new Exception("请重新登录");
-            return Decode(request.Headers["token"], key);
-            
-
+        /// <summary>
+        /// 校验是否登录过
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        internal static object ValideLogined(HttpRequestHeaders headers)
+        {
+            if (headers.GetValues("token") == null || !headers.GetValues("token").Any())
+            {
+                throw new Exception("请登陆");
+            }
+            return Decode(headers.GetValues("token").First());
         }
     }
 }
